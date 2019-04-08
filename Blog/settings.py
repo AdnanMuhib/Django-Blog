3f167d3460df1591ce3418288fb0a5f6ffefc041
Adnan <adnan.muhib@rocketmail.com>
2019-04-09 01:43:39 +0500

    Update: Database settings and run migration, ADD: new Post App inside the project

--- Blog/settings.py
+++ Blog/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog_db',
+        'USER': 'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
